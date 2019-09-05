7069468c58f8433be1e4099f0ea588436511e525
lrchang2 <lucychang84@hotmail.com>
2019-09-05 12:03:32 -0500

    Update 4HOnline_location_cleaning.py

--- 4HOnline_location_cleaning.py
+++ 4HOnline_location_cleaning.py
@@ -11,9 +11,13 @@ def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
 def containsCommas(inputString):
     return any(char=="," for char in inputString)
+def hasHashtags(inputString):
+    return any(char=="#" for char in inputString)
 
 #psuedo dictionary
 dict={'Adams': ['Camp Point','Mendon','Quincy', 'Hull'], 'Bond': ['Greenville', 'Kinmundy', 'Champaign', 'Sorento']}
+road=pd.read_csv("/Users/Lucy/Desktop/RoadTypes.csv")
+roads=road.to_dict()
 
 #sort 'Program: Location' into PEARS attributes
 for index, location in enumerate(df['Program: Location']):
@@ -28,7 +32,20 @@ for index, location in enumerate(df['Program: Location']):
                 df.loc[index,'Program Name'] = location
 
 #clean 'Program Address' for consistency
+#splits location in "address", "city" and "name" (not in that order)
+split_addresses=[]
+split_addresses_index=[]
 for index, address in enumerate(df['Program Address']):
     if containsCommas(address) == True:
-        #a = address.split(',')
-        df.at[index,'Program Address']= address.split(',')
+        split_addresses_index.append(index)
+        split_addresses.append(address.split(','))
+
+#
+for i, x in enumerate(a):
+    if hasNumbers(x[0]) == True:
+        df.loc[b[i],'Program Address']= a[i][0]
+        df.loc[b[i],'Program City']= a[i][1]
+    else:
+        df.loc[b[i],'Program Address']= a[i][1]
+        if len(a[i]) > 2: 
+            df.loc[b[i],'Program City']= a[i][2]
